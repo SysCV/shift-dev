d6a860c3ee2be487a1733e01361689cd04597317
suniique <taosun47@student.ethz.ch>
2022-10-11 15:28:48 +0200

    remove dependency of pycocotools

--- shift_dev/utils/storage.py
+++ shift_dev/utils/storage.py
@@ -3,15 +3,6 @@ import io
 import tarfile
 import zipfile
 
-import numpy as np
-
-try:
-    from pycocotools import mask as mask_utils  # type: ignore
-except Exception as e:
-    print("Error during importing pycocotools:", e, "\nconsider to reinstall the pycocotools manually.")
-
-from ..types.scalabel import RLE
-
 
 class ZipArchiveReader:
     def __init__(self, filename) -> None:
@@ -53,12 +44,3 @@ class TarArchiveReader:
 def string_hash(video):
     sha = hashlib.sha512(video.encode("utf-8"))
     return int(sha.hexdigest(), 16)
-
-
-def mask_to_rle(mask) -> RLE:
-    """Converting mask to RLE format."""
-    assert 2 <= len(mask.shape) <= 3
-    if len(mask.shape) == 2:
-        mask = mask[:, :, None]
-    rle = mask_utils.encode(np.array(mask, order="F", dtype="uint8"))[0]
-    return RLE(counts=rle["counts"].decode("utf-8"), size=rle["size"])
